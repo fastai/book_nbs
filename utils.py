05e9e8c655d942c682da967a704bbab2f8897955
Sylvain Gugger <sylvain.gugger@gmail.com>
2020-02-25 14:08:31 -0800

    Update

--- utils.py
+++ utils.py
@@ -1,4 +1,3 @@
-# +
 # Numpy and pandas by default assume a narrow screen - this fixes that
 from fastai2.vision.all import *
 from nbdev.showdoc import *
@@ -8,7 +7,7 @@ from pandas.api.types import CategoricalDtype
 import matplotlib as mpl
 # mpl.rcParams['figure.dpi']= 200
 mpl.rcParams['savefig.dpi']= 200
-# -
+mpl.rcParams['font.size']=12
 
 set_seed(42)
 torch.backends.cudnn.deterministic = True
